9a3d8e081168441711f4e1396789b71a30da1bb1
Ulaş Hasbay <ulashasbay@gmail.com>
2021-11-21 16:19:07 +0300

    single post pages created

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.get('/', async (req, res) => {
     });  
 });
 
+// Single post pages
+app.get('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post', {
+        post
+    });  
+});
+
 app.get('/about', (req, res) => {
     res.render('about');  
 });
